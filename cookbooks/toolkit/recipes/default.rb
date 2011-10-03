c5015f0bde28f903e3f89e018aeaf16566ba56cc
Andy Allan <andy@gravitystorm.co.uk>
2011-10-03 14:38:16 +0100

    Use gem1.9.1 to install bundle

--- cookbooks/toolkit/recipes/default.rb
+++ cookbooks/toolkit/recipes/default.rb
@@ -6,6 +6,19 @@
 
 include_recipe 'passenger'
 include_recipe 'postgres'
+include_recipe 'ruby19'
+
+# We can install bundler with the ubuntu version of gem ...
+gem_package "bundler" do
+  gem_binary "/usr/bin/gem1.9.1"
+  action :install
+end
+
+# ... but it installs binaries into a non-PATH directory
+# See http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=448639
+link "/usr/bin/bundle" do
+  to "/var/lib/gems/1.9.1/bin/bundle"
+end
 
 directory "/var/www/toolkit/shared" do
   owner "www-data"
