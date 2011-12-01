9f4c729f461af0559e1c263f6ee8e2b54403b6a3
Andrew France <andrew@avito.co.uk>
2011-12-01 12:49:15 +0000

    Change chef logging level to debug and output to log file.

--- solo.rb
+++ solo.rb
@@ -2,3 +2,6 @@ root = File.expand_path(File.dirname(__FILE__))
 
 cookbook_path root + '/cookbooks'
 data_bag_path '/etc/chef/databags'
+log_level :debug
+log_location '/var/log/chef/solo.log'
+verbose_logging true
