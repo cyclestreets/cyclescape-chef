25b289edebb422c133ad5b5eb5d44b70459bee9d
Andy Allan <andy@gravitystorm.co.uk>
2012-11-01 09:40:07 +0000

    Upgrade to postgres 9.1, which simplifies things hugely.

--- cookbooks/postgres/recipes/default.rb
+++ cookbooks/postgres/recipes/default.rb
@@ -4,20 +4,11 @@
 #
 # Copyright 2011, Cyclestreets Ltd
 
-apt_repository "pitti" do
-  uri "http://ppa.launchpad.net/pitti/postgresql/ubuntu"
-  distribution node['lsb']['codename']
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-  key "8683D8A2"
-  action :add
-end
-
-package "postgresql-9.0" do
+package "postgresql-9.1" do
   action :install
 end
 
-package "postgresql-contrib-9.0" do
+package "postgresql-contrib-9.1" do
   action :install
 end
 
@@ -26,28 +17,7 @@ service "postgresql" do
   action :enable
 end
 
-# https://launchpad.net/~ubuntugis/+archive/ubuntugis-unstable
-# purely for libgeos-c1 needed for postgis
-apt_repository "ubuntugis-stable" do
-  uri "http://ppa.launchpad.net/ubuntugis/ppa/ubuntu"
-  distribution node['lsb']['codename']
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-  key "314DF160"
-  action :add
-end
-
-# https://launchpad.net/~pi-deb/+archive/gis
-apt_repository "pi-deb" do
-  uri "http://ppa.launchpad.net/pi-deb/gis/ubuntu"
-  distribution node['lsb']['codename']
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-  key "226213AA"
-  action :add
-end
-
-package "postgresql-9.0-postgis" do
+package "postgresql-9.1-postgis" do
   action :install
 end
 
