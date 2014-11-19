03c9a3f1f02597028e3cc6de09fad1d8f0dde257
Andy Allan <andy@gravitystorm.co.uk>
2014-11-19 14:18:38 +0000

    Add a chefspec test to ensure the version number ends up in the template.

---
+++ cookbooks/passenger-gem/spec/default_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'passenger-gem::default' do
+  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+
+  before do
+    stub_command("/usr/sbin/apache2 -t").and_return(true)
+    stub_command("test -f /var/lib/gems/1.9.1/gems/passenger-3.0.9/ext/apache2/mod_passenger.so").and_return(false)
+  end
+
+  it 'should create a template with the correct version' do
+    expect(chef_run).to render_file('/etc/apache2/mods-available/passenger.load')
+      .with_content('LoadModule passenger_module /var/lib/gems/1.9.1/gems/passenger-3.0.9/ext/apache2/mod_passenger.so')
+  end
+end
