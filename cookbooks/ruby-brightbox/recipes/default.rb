0039594deea83a2bed3290e7db13ef5cb1e578a6
Nikolai B <nikolai.b.dev@gmail.com>
2015-05-06 22:46:06 +0100

    fix brightbox ppa

--- cookbooks/ruby-brightbox/recipes/default.rb
+++ cookbooks/ruby-brightbox/recipes/default.rb
@@ -2,18 +2,17 @@
 # Cookbook Name:: ruby-brightbox
 # Recipe:: default
 #
-# Copyright 2011, Cyclestreets Ltd
-
-# I hate having multiple rubies, but I hate having rvm even more
-
-# Due to the joys of debian package names, these are actually 1.9.2
+# Copyright 2015, Cyclestreets Ltd
 
 apt_repository "ruby-ng" do
-  uri "https://launchpad.net/~brightbox/+archive/ubuntu/ruby-ng"
+  uri "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu"
   components ["main"]
+  distribution 'trusty'
   keyserver "keyserver.ubuntu.com"
-  key "80F70E11F0F0D5F10CB20E62F5DA5F09C3173AA6"
+  key "C3173AA6"
   action :add
 end
 
 package 'ruby2.1'
+package 'ruby2.1-dev'
+
