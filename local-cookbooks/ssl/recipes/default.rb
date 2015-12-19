4283c89328e0df96cc1bfa7a1cc0abfa2e5ee225
mvl22 <martin@lucas-smith.co.uk>
2015-12-19 20:33:19 +0000

    Update SSL config away from Startcom

--- local-cookbooks/ssl/recipes/default.rb
+++ local-cookbooks/ssl/recipes/default.rb
@@ -7,7 +7,7 @@
 # This recipe sets up some dummy SSL certificates,
 # so that apache will load without complaining.
 
-# Install the actual startcom certificates used for
+# Install the actual intermediate CA certificates used for
 # the live operations
 
 cert_dir = '/etc/apache2/ssl/'
@@ -19,9 +19,8 @@ directory cert_dir do
 end
 
 certs = {
-  # Files obtained from http://www.startssl.com/certs/
-  startcom_ca: 'startcom.ca.cer',
-  startcom_server: 'startcom.sub.class2.server.ca.cer',
+  # Intermediate CA certificate
+  ca: 'ca.ssl.cer',
 
   # These are dummy, self-signed certificates that need to
   # be replaced on the live server
