cdc4956cf45724f4bc755667a5608ddba0860a2d
Nikolai B <nikolai-b@users.noreply.github.com>
2016-01-09 18:58:12 +0000

    Bump rubygems version
    https://github.com/rubygems/rubygems/issues/853 was causing
    rack 2.0.0-alpha to be installed incorrectly

--- local-cookbooks/cyclescape/attributes/default.rb
+++ local-cookbooks/cyclescape/attributes/default.rb
@@ -14,6 +14,7 @@ default['apache']['listen_ports'] = %w(80 443)
 default['passenger']['ruby_bin'] = "/usr/bin/ruby#{default['brightbox-ruby']['version']}"
 default['passenger']['version'] = '5.0.20'
 default['brightbox-ruby']['install_ruby_switch'] = true
+default['brightbox-ruby']['rubygems_version'] = '2.5.1'
 default['java']['jdk_version'] = 7
 
 default['apache']['prefork']['startservers'] = 5
