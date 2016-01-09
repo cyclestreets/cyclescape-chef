5a11af409868b51593b09dfe241e5e5b47fa7302
Nikolai B <nikolai-b@users.noreply.github.com>
2016-01-09 20:29:37 +0000

    Fix passenger root_path
    The version change wasn't being pulled in

--- local-cookbooks/cyclescape/attributes/default.rb
+++ local-cookbooks/cyclescape/attributes/default.rb
@@ -13,6 +13,7 @@ default['firewall']['rules'] = [
 default['apache']['listen_ports'] = %w(80 443)
 default['passenger']['ruby_bin'] = "/usr/bin/ruby#{default['brightbox-ruby']['version']}"
 default['passenger']['version'] = '5.0.20'
+default['passenger']['root_path'] = "#{languages['ruby']['gems_dir']}/gems/passenger-#{node['passenger']['version']}"
 default['brightbox-ruby']['install_ruby_switch'] = true
 default['brightbox-ruby']['rubygems_version'] = '2.5.1'
 default['java']['jdk_version'] = 7
