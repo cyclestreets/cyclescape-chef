392b78cb4ed666b1a97b7cf88e2d0d5d1b16acd0
Nikolai B <nikolai-b@users.noreply.github.com>
2017-11-25 11:31:15 +0000

    Bump passenger

--- local-cookbooks/cyclescape/attributes/default.rb
+++ local-cookbooks/cyclescape/attributes/default.rb
@@ -19,5 +19,5 @@ default['java']['jdk_version'] = 7
 
 default['apache']['prefork']['startservers'] = 4
 default['passenger']['ruby_bin'] = "/usr/bin/ruby#{default['brightbox-ruby']['version']}"
-default['passenger']['version'] = '5.1.3'
+default['passenger']['version'] = '5.1.11'
 default['passenger']['root_path'] = "#{languages['ruby']['gems_dir']}/gems/passenger-#{node['passenger']['version']}"
