33355ddb6a2f6a1fa196bc54b3a953994152af06
Nikolai B <nikolai-b@users.noreply.github.com>
2017-11-25 11:10:03 +0000

    Set ruby version BEFORE using it

--- local-cookbooks/cyclescape/attributes/default.rb
+++ local-cookbooks/cyclescape/attributes/default.rb
@@ -11,9 +11,6 @@ default['firewall']['rules'] = [
 ]
 
 default['apache']['listen_ports'] = %w(80 443)
-default['passenger']['ruby_bin'] = "/usr/bin/ruby#{default['brightbox-ruby']['version']}"
-default['passenger']['version'] = '5.1.3'
-default['passenger']['root_path'] = "#{languages['ruby']['gems_dir']}/gems/passenger-#{node['passenger']['version']}"
 default["brightbox-ruby"]["version"] = "2.3"
 default['brightbox-ruby']['default_action'] = :install
 default['brightbox-ruby']['install_ruby_switch'] = true
@@ -21,3 +18,6 @@ default['brightbox-ruby']['rubygems_version'] = '2.6.13'
 default['java']['jdk_version'] = 7
 
 default['apache']['prefork']['startservers'] = 4
+default['passenger']['ruby_bin'] = "/usr/bin/ruby#{default['brightbox-ruby']['version']}"
+default['passenger']['version'] = '5.1.3'
+default['passenger']['root_path'] = "#{languages['ruby']['gems_dir']}/gems/passenger-#{node['passenger']['version']}"
