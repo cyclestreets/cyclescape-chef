5947001d8c2c8e2d7466672b53efb4afe66c67a8
Nikolai B <nikolai-b@users.noreply.github.com>
2019-04-02 21:20:57 +0100

    We due need ruby-switch after all

--- local-cookbooks/cyclescape/recipes/default.rb
+++ local-cookbooks/cyclescape/recipes/default.rb
@@ -17,10 +17,6 @@ end
 
 include_recipe 'java'
 
-if node['platform_version'] == '16.04'
-  node.default['brightbox-ruby']['install_ruby_switch'] = false
-end
-
 include_recipe 'brightbox-ruby::default'
 gem_package 'rack' do
   action :install
