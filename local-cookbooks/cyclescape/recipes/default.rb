312f42ac248643df105319416ef11b8aba89cf04
Nikolai B <nikolai-b@users.noreply.github.com>
2019-07-08 22:30:04 +0100

    Install npm explicitly
    No idea why the chef cookbook doesn't do this

--- local-cookbooks/cyclescape/recipes/default.rb
+++ local-cookbooks/cyclescape/recipes/default.rb
@@ -32,6 +32,8 @@ package 'nodejs' do
   action :upgrade
 end
 
+package 'npm'
+
 deploy_dir = '/var/www/cyclescape'
 shared_dir = File.join(deploy_dir, 'shared')
 node.default['letsencrypt']['error_email'] = data_bag_item("secrets", "i18n")["error_email"]
