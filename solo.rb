8363c7052bee863820131da1451bda482df3f4f8
Nikolai B <nikolai-b@users.noreply.github.com>
2019-08-04 09:42:34 +0100

    Create log dir if missing

--- solo.rb
+++ solo.rb
@@ -1,7 +1,9 @@
+require 'fileutils'
 root = File.expand_path(File.dirname(__FILE__))
 
 cookbook_path root + '/cookbooks'
 data_bag_path '/etc/chef/databags'
 log_level :debug
 log_location '/var/log/chef/solo.log'
+FileUtils.mkdir_p File.dirname(log_location)
 verbose_logging true
