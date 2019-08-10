6f635449d26554577bd83f1b800152025bc3bfcd
Nikolai B <nikolai-b@users.noreply.github.com>
2019-08-10 09:23:07 +0100

    Reduce chef log level

--- solo.rb
+++ solo.rb
@@ -3,7 +3,7 @@ root = File.expand_path(File.dirname(__FILE__))
 
 cookbook_path root + '/cookbooks'
 data_bag_path '/etc/chef/databags'
-log_level :debug
+log_level :info
 log_location '/var/log/chef/solo.log'
 FileUtils.mkdir_p File.dirname(log_location)
 verbose_logging true
