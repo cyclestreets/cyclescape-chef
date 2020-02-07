a8969890da05c86ffea8a91f5980392e85997464
Nikolai B <nikolai-b@users.noreply.github.com>
2020-02-07 21:49:01 +0000

    Add pg_stat_statments

--- local-cookbooks/postgres/recipes/default.rb
+++ local-cookbooks/postgres/recipes/default.rb
@@ -31,3 +31,14 @@ end
 package postgis_pkg do
   action :install
 end
+
+pg_config_file = "/etc/postgresql/#{pg_version}/main/postgresql.conf"
+bash "Add pg_stat_statements to pg_config_file" do
+  code <<-EOL
+    echo "shared_preload_libraries = 'pg_stat_statements'
+pg_stat_statements.track = all" >> #{pg_config_file}
+  EOL
+  only_if do
+    ::File.exists?(pg_config_file) && !File.foreach(pg_config_file).grep(/pg_stat_statements\.track = all/).any?
+  end
+end
