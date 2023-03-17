122030c34871667ff8d00991d2cf0635a48b20c6
Nikolai B <nikolai-b@users.noreply.github.com>
2023-03-17 14:05:45 +0000

    Remove postfix config

--- local-cookbooks/postfix/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'postfix'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures munin'
-long_description ''
-version          '0.0.1'
