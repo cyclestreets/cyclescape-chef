cc75a21c2fbd948858a825788bad1346fe06cfc4
Nikolai B <nikolai-b@users.noreply.github.com>
2024-09-18 10:58:41 +0100

    Bump passenger version

--- local-cookbooks/cyclescape/metadata.rb
+++ local-cookbooks/cyclescape/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Cyclestreets Ltd'
 maintainer_email 'YOUR_EMAIL'
 license          'MIT'
 description      'Installs/Configures Cyclescape'
-version          '0.4.1'
+version          '0.4.2'
 
 depends 'apt'
 depends 'ssl'
